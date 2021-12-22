843888c7dccf49ecd974d774c488510b10b32e6e
Magda19P <90355852+Magda19P@users.noreply.github.com>
2021-12-22 09:07:40 +0100

    Create Tests_with_adresat_menedzer.cpp
    Tests checking if Adresat is correctly removed or edited.

---
+++ Tests_with_adresat_menedzer.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include "KsiazkaAdresowa.h"
+
+
+using namespace std;
+
+int _main()
+{
+    KsiazkaAdresowa ksiazkaAdresowa("Uzytkownicy.txt", "Adresaci.txt");
+
+
+    ksiazkaAdresowa.wypiszWszystkichUzytkownikow();
+
+    ksiazkaAdresowa.logowanieUzytkownika();
+
+    ksiazkaAdresowa.dodajAdresata();
+
+    ksiazkaAdresowa.wyswietlWszystkichAdresatow();
+
+
+   // ksiazkaAdresowa.zmianaHaslaZalogowanegoUzytkownika();
+
+    //ksiazkaAdresowa.wylogowanieUzytkownika();
+
+    return 0;
+}
+
+//TESTY ADRESAT_MENEDZER
+#include "AdresatMenedzer.h"
+int main()
+{
+    AdresatMenedzer adresatMenedzer("Adresaci_test.txt", 2);
+
+    adresatMenedzer.dodajAdresata();
+    adresatMenedzer.edytujAdresata();
+    adresatMenedzer.wyswietlWszystkichAdresatow();
+    adresatMenedzer.usunAdresata();
+    adresatMenedzer.wyswietlWszystkichAdresatow();
+
+    return 0;
+}
